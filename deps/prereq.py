fe7d26a659d2d0cb72cc483330c1877a9ccc64bb
juggernaut09 <venkatteja.ravi@gmail.com>
2022-03-02 15:19:27 +0530

    feat: prereq.py

--- deps/prereq.py
+++ deps/prereq.py
@@ -17,13 +17,14 @@ else:
     subprocess.run(['sudo', 'apt', 'install', 'build-essential', 'jq', '-y'])
     subprocess.run(['wget', 'https://dl.google.com/go/go1.17.3.linux-amd64.tar.gz'])
     subprocess.run(['tar', '-xvf', 'go1.17.3.linux-amd64.tar.gz'])
-    subprocess.run(['sudo', 'mv', 'go', '/usr/local'])
     subprocess.run(['rm', 'go1.17.3.linux-amd64.tar.gz'])
+
     print("------ Update bashrc ---------------")
-    os.environ['GOPATH'] = '{}/go'.format(os.environ['HOME'])
+    os.environ['GOPATH'] = '{}/go'.format(os.getenv(key = 'HOME'))
     os.environ['GOROOT'] = '/usr/local/go'
-    os.environ['GOBIN'] = '{}/bin'.format(os.environ['GOPATH'])
-    os.environ['PATH'] = '{}:/usr/local/go/bin:{}'.format(os.environ['PATH'], os.environ['GOBIN'])
+    os.environ['GOBIN'] = '{}/bin'.format(os.getenv(key = 'GOPATH'))
+    subprocess.run(['sudo', 'mkdir', '-p', '{}'.format(os.getenv(key = 'GOBIN'))])
+    subprocess.run(['sudo', 'mkdir', '-p', '{}/src/github.com'.format(os.getenv(key = 'GOPATH'))])
     bash_object = open('{}/.bashrc'.format(os.environ['HOME']), 'a')
     bash_object.write('')
     bash_object.write('export GOPATH=$HOME/go\n')
@@ -31,10 +32,6 @@ else:
     bash_object.write('export GOBIN=$GOPATH/bin\n')
     bash_object.write('export PATH=$PATH:/usr/local/go/bin:$GOBIN\n')
     bash_object.close()
-    subprocess.run(['.', '{}/.bashrc'.format(os.environ['HOME'])])
-    subprocess.run(['sudo', 'mkdir', '-p', '{}'.format(os.environ['GOBIN'])])
-    subprocess.run(['sudo', 'mkdir', '-p', '{}/src/github.com'.format(os.environ['GOPATH'])])
-    subprocess.run(['go', 'version'])
 
 
 
